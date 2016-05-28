cc7bffb1be3f7845ec7ac931044d8c51864857ac
Tim Head <betatim@gmail.com>
2016-05-28 17:59:43 +0200

    Mmmmm bytes

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ class EmailBoss(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type','text/html')
             self.end_headers()
-            self.wfile.write("Hello World!")
+            self.wfile.write(b"Hello World!")
             return
 
         else:
